774a328c51ff81666dc133416671d93f21216bc4
Marcel Rouwenhorst <marcel@cammio.com>
2017-08-04 15:03:53 +0200

    firefox 52 ESR fix

--- ice.c
+++ ice.c
@@ -2221,7 +2221,7 @@ static void janus_ice_cb_nice_recv(NiceAgent *agent, guint stream_id, guint comp
 				JANUS_LOG(LOG_DBG, "%p remote SSRC: video=%u, audio=%u, video:%i, audio:%i\n ", stream, stream->video_ssrc_peer, stream->audio_ssrc_peer, stream->video_ssrc_peer_bye_received, stream->audio_ssrc_peer_bye_received);
 				if(janus_rtcp_has_bye(buf, buflen, stream->video_ssrc_peer, stream->audio_ssrc_peer, &stream->video_ssrc_peer_bye_received, &stream->audio_ssrc_peer_bye_received)) {
 					JANUS_LOG(LOG_VERB, "[%"SCNu64"] Got RTCP BYE on stream %"SCNu16" (component %"SCNu16"), closing...\n", handle->handle_id, stream->stream_id, component->component_id);
-					// janus_ice_webrtc_hangup(handle, "RTCP BYE");
+					janus_ice_webrtc_hangup(handle, "RTCP BYE");
 					return;
 				}
 				/* Is this audio or video? */
